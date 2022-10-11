406b130bee397c2aabae4f5f75d8532e2a78a332
melkuz <f-x-colombia@hotmail.com>
2022-10-11 11:23:21 -0400

    mise en forme

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title:"Lab 04", h1:"Me superbe Apllication Express", p:"Les membre de l\'équipe est : felix alberto"});
 });
 
 module.exports = router;
