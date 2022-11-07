c817a1076f19321109cad11a6805505196308b93
melkuz <50109008+melkuz@users.noreply.github.com>
2022-11-06 21:34:34 -0500

    ajout de sql

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodejs'
+})
 
+connection.connect()
+
+connection.query('SELECT * from voitures', (err, result, fields) => {
+  if (err) throw err
+
+  console.log(result)
+})
+
+connection.end()
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title:"Lab 04", h1:"Me superbe Apllication Express", p:"Les membre de l\'équipe est : felix alberto"});
+  res.render('index', { title:"NodeJs", h1:"Les voitures sur place", p:"Un site pour voir des voitures et ses characteristiques"});
 });
 
 module.exports = router;
